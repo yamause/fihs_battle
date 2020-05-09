9de3359f8dfa6254d1aeb658207ad2ca83655019
yamause <keitadep@gmail.com>
2020-05-09 14:52:54 +0900

    Update Battle Test

--- Test_Code/Battle_test.py
+++ Test_Code/Battle_test.py
@@ -1,42 +1,144 @@
-# coding: utf-8
-# Your code here!
 import random
+import logging
+formatter = '%(levelname)s : location:%(pathname)s(%(lineno)s) MSG__ %(message)s'
+logging.basicConfig(level=logging.DEBUG,format=formatter)
+logger = logging.getLogger(__name__)
+
 class char():
-    enemy_box = []
+    #味方初期値の設定
     def __init__(self,name,life,power,defense):
-        char.enemy_box.append(self)
         self.name = name
         self.life = life
         self.power = power
         self.defense = defense
-        
-    def battle(self):
-        myfish_power = my_fish.power - self.defense + int(random.randint(-5,10))
-        emfish_power = self.power - my_fish.defense + int(random.randint(-5,10))
+
+class enemy_char(char):
+    #敵初期値の設定
+    enemy_box = []
+    def __init__(self,name,life,power,defense):
+        super().__init__(name,life,power,defense)
+        enemy_char.enemy_box.append(self)
+
+class janken():
+    @classmethod
+    def main(cls,my_cmd,enemy_cmd):
+
+        cls.enemy_cmd = enemy_cmd
+        cls.my_cmd = my_cmd
+
+        logger.debug("自分の手　" + str(cls.my_cmd))
+        logger.debug("相手の手　" + str(cls.enemy_cmd))
+
+        #ジャンケン処理
+        if cls.enemy_cmd == cls.my_cmd:
+            cls.winner = "drow"
+            return(cls.winner)
+        elif (
+            cls.enemy_cmd == 0 and cls.my_cmd == 1 or 
+            cls.enemy_cmd == 1 and cls.my_cmd == 2 or 
+            cls.enemy_cmd == 2 and cls.my_cmd == 0
+            ):
+            cls.winner = "enemy"
+            return(cls.winner)
+        else :
+            cls.winner = "my_fish"
+            return(cls.winner)
+                
+class calc():
+    @classmethod
+    def calc_method(cls,winner,my_p,my_d,my_l,enemy,en_p,en_d,en_l):
+        if winner == "my_fish":
+            my_p *= 1.5
+        elif winner == "enemy":
+            en_p *= 1.5
+        logger.debug(("{}:{}").format(my_p,winner))
+        logger.debug(en_p)
+        myfish_power = my_p - en_d + random.randint(-5,10)
+        emfish_power = en_p - my_d + random.randint(-5,10)
         myfish_power = 0 if myfish_power <= 0 else myfish_power
         emfish_power = 0 if emfish_power <= 0 else emfish_power
         
-        my_fish.life -= emfish_power
-        self.life -= myfish_power
-        print("--------------------------------------------")
-        print("●"+my_fish.name+ "に" + str(emfish_power) + "のダメージ!残りは"+str(my_fish.life)+"！")
-        print("●"+self.name + "に" + str(myfish_power) + "のダメージ!残りは"+str(self.life)+"！")
-        print("--------------------------------------------")
+        my_l -= emfish_power
+        en_l -= myfish_power
         
-class char_1():
-    def __init__(self,name,life,power,defense):
-        self.name = name
-        self.life = life
-        self.power = power
-        self.defense = defense
+        logger.debug(("{}:{}").format(emfish_power,myfish_power))
+        logger.debug(("{}:{}").format(my_l,en_l))
+        return(int(my_l),int(en_l))
 
-#my_fish = char(my,attr["life"],attr["power"],attr["defence"])
-my_fish = char_1("ミカタ",100,100,100)
+#--------------------------------------------------------
 
-enemy_fish_1 = char("ザコテキ",100,100,100)
-enemy_fish_2 = char("フツテキ",150,150,150)
-enemy_fish_3 = char("ツヨテキ",200,200,200)
+def Test_code(pers_attr,sess_attr,slots_id):
+    if not sess_attr:
+    
+        sess_attr["round"] = 0
+        sess_attr["my_status"] = {}
+        sess_attr["en_status"] = {}
+        my = "myfish"
+        my_fish = (
+            char(
+                my,
+                pers_attr["life"],
+                pers_attr["power"],
+                pers_attr["defence"]
+                    )
+        )
+        enemy_1 = enemy_char("ザコテキ",100,100,100),
+        enemy_2 = enemy_char("フツテキ",150,150,150),
+        enemy_3 = enemy_char("ツヨテキ",200,200,200)
+    
+                # 敵のランダム出現
+        num = random.randint(0,len(enemy_char.enemy_box) - 1)
+        enemy_obj = enemy_char.enemy_box[num]
+    
+                
+    
+        # 相手と自分のコマンド入力
+        my_cmd = int(slots_id)
+        enemy_cmd = random.randint(0,2)
+        
+        winner = janken.main(my_cmd,enemy_cmd)
+        logger.debug(winner)
+    
+        # バトル計算
+        my_l,en_l = calc.calc_method(
+                        winner,
+                        my_fish.life,
+                        my_fish.power,
+                        my_fish.defense,
+                        enemy_obj.name,
+                        enemy_obj.life,
+                        enemy_obj.power,
+                        enemy_obj.defense
+                        )
+    
+        if my_l <= 0:
+            speak_output = ("{}との戦いに敗北しました！").format(enemy_obj.name)
+            bools = True
+    
+        elif en_l <= 0:
+            pers_attr["v_count"] += 1
+            speak_output = ("{}との戦いに勝利しました！").format(enemy_obj.name)
+            bools = False
+            
+        else :
+            sess_attr["round"] += 1
+            sess_attr["my_status"] = my_l
+            sess_attr["en_status"] = enemy_obj.life,enemy_obj.power,enemy_obj.defense
+            speak_output = ("次の戦闘に移ります")
+            bools = True
+    
+       
+        speak_output += ("自分のライフ:{},相手のライフ:{}".format(my_l,en_l))
+        return(speak_output)
+    
 
-for i in range(10):
-    num = random.randint(0,len(char.enemy_box) - 1)
-    char.enemy_box[num].battle()
+#--------------------------------------------------------
+
+# Persistent Attributes を変数に代入
+pers_attr = {"life":100,"power":100,"defence":100}
+# Session Attributes を変数に代入
+sess_attr = {}
+# スロット値の取得
+slots_id = 0
+
+logger.debug(("{}").format(Test_code(pers_attr,sess_attr,slots_id)))
--- lambda/py/handler/handler_battle.py
+++ lambda/py/handler/handler_battle.py
@@ -19,11 +19,6 @@ class BattleIntentHandler(AbstractRequestHandler):
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
 
-        pers_attr = handler_input.attributes_manager.persistent_attributes
-        sess_attr = handler_input.attributes_manager.session_attributes
-        sess_attr["round"] = 0
-
-        #クラス
         class char():
             #味方初期値の設定
             def __init__(self,name,life,power,defense):
@@ -32,7 +27,6 @@ class BattleIntentHandler(AbstractRequestHandler):
                 self.power = power
                 self.defense = defense
 
-
         class enemy_char(char):
             #敵初期値の設定
             enemy_box = []
@@ -40,63 +34,123 @@ class BattleIntentHandler(AbstractRequestHandler):
                 self().__init__(name,life,powerf,defense):
                 char.enemy_box.append(self)
 
-        class battle():
-            #バトルの処理
+        class janken():
             @classmethod
-            def battle(cls,enemy,my_cmd):
-                
+            def main(cls,my_cmd,enemy_cmd):
+
                 # グー      ０ 
                 # チョキ    １ 
                 # パー      ２
-
                 #相手のジャンケンコマンド
-                cls.enemy_cmd = random.randint(0,2)
-                print(cls.enemy_cmd)
+
+                cls.enemy_cmd = enemy_cmd
+                cls.my_cmd = my_cmd
+
+                print("自分の手　" + str(cls.my_cmd))
+                print("相手の手　" + str(cls.enemy_cmd))
 
                 #ジャンケン処理
                 if cls.enemy_cmd == cls.my_cmd:
-                    winner = "aiko"
-                    print(winner)
+                    cls.winner = "drow"
+                    return(cls.winner)
                 elif (
                     cls.enemy_cmd == 0 and cls.my_cmd == 1 or 
                     cls.enemy_cmd == 1 and cls.my_cmd == 2 or 
-                    cls.enemy_cmd == 2 and cls.my_cmd == 0:
-                    )
+                    cls.enemy_cmd == 2 and cls.my_cmd == 0
+                    ):
                     cls.winner = "enemy"
-                    print(cls.winner)
+                    return(cls.winner)
                 else :
                     cls.winner = "my_fish"
-                    print(cls.winner)
                 
-                myfish_power = my_fish.power - enemy.defense + int(random.randint(-5,10))
-                emfish_power = enemy.power - my_fish.defense + int(random.randint(-5,10))
-                myfish_power = 0 if myfish_power <= 0 else myfish_power
-                emfish_power = 0 if emfish_power <= 0 else emfish_power
+                return(cls.winner)
+                
+        class calc():
+            @classmethod
+            def calc_method(cls,winner,my_p,my_d,my_l,en_p,en_d,en_l):
+                if winner == my_fish:
+                    my_p *= 1.5
+                elif winner == enemy:
+                    en_p *= 1.5
+
+                myfish_power = my_p - en_d + random.randint(-5,10)
+                emfish_power = en_p - my_d + random.randint(-5,10)
+                myfish_power = 0 if my_p <= 0 else my_p
+                emfish_power = 0 if en_p <= 0 else en_p
         
-                my_fish.life -= emfish_power
-                self.life -= myfish_power
-                print("--------------------------------------------")
-                print("●"+my_fish.name+ "に" + str(emfish_power) + "のダメージ!残りは"+str(my_fish.life)+"！")
-                print("●"+self.name + "に" + str(myfish_power) + "のダメージ!残りは"+str(self.life)+"！")
-                print("--------------------------------------------")
+                my_l -= emfish_power
+                en_l -= myfish_power
 
+                return(my_l,en_l)
 
-        class battle_2(battle):
+#--------------------------------------------------------
 
+        pers_attr = handler_input.attributes_manager.persistent_attributes
+        sess_attr = handler_input.attributes_manager.session_attributes
 
-        if sess_attr["round"] == 0:
-            my_fish = char(my,pers_attr["life"],pers.attr["power"],pers.attr["defence"])
-            
-            enemy_fish_1 = char("ザコテキ",100,100,100),
-            enemy_fish_2 = char("フツテキ",150,150,150),
-            enemy_fish_3 = char("ツヨテキ",200,200,200)
+        # スロット値の取得
+        slots = handler_input.request_envelope.request.intent.slots
+        slots_id = slots["janken_slot"].resolutions.resolutions_per_authority[0].values[0].value.id
+
+        if not sess_attr["round"]:
+
+            sess_attr["round"] = 0
+            sess_attr["my_status"] = {}
+            sess_attr["en_status"] = {}
+
+            my_fish = (
+                char(
+                    my,
+                    pers_attr["life"],
+                    pers.attr["power"],
+                    pers.attr["defence"]
+                )
+            )
+            enemy_1 = enemy_char("ザコテキ",100,100,100),
+            enemy_2 = enemy_char("フツテキ",150,150,150),
+            enemy_3 = enemy_char("ツヨテキ",200,200,200)
 
             # 敵のランダム出現
-            num = random.randint(0,len(char.enemy_box) - 1)
-            char.enemy_box[num].name
+            num = random.randint(0,len(enemy_char.enemy_box) - 1)
+            enemy_obj = enemy_char.enemy_box[num]
 
-            #ラウンド分岐
-            char.enemy_box[num].battle()
+            
+
+            # 相手と自分のコマンド入力
+            my_cmd = int(slots_id)
+            enemy_cmd = random.randint(0,2)
+            
+            winner = janken.main(my_cmd,enemy_cmd)
+
+            # バトル計算
+            my_l,en_l = calc.calc_method(
+                            winner,
+                            my_fish.life,
+                            my_fish.power,
+                            my_fish.defense,
+                            enemy_obj.life,
+                            enemy_obj.power,
+                            enemy_obj.defense
+                            )
+
+            if my_l <= 0:
+                handler_input.attributes_manager.delete_persistent_attributes()
+                speak_output = ("あなたのフィッシュのライフが0になり、死んでしまいました。また初めから遊んでください。")
+                bools = True
+
+            elif en_l <= 0:
+                pers_attr["v_count"] += 1
+                speak_output = ("{}との戦いに勝利しました！").format(enemy_obj.name)
+                bools = False
+
+            sess_attr["round"] += 1
+            sess_attr["my_status"] = my_l
+            sess_attr["en_status"] = enemy_obj.life,enemy_obj.power,enemy_obj.defense
+            speak_output = ("")
+            bools = True
+
+
+
 
         elif sess_attr["round"] == 1 :
             my_fish = char(my,pers_attr["life"],pers.attr["power"],pers.attr["defence"])
@@ -146,6 +200,6 @@ class BattleIntentHandler(AbstractRequestHandler):
         return (
             handler_input.response_builder
             .speak(speak_output)
-            .set_should_end_session(False)
+            .set_should_end_session(bools)
             .response
         )
