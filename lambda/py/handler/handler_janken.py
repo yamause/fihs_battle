34c1c5f14212dfbc54c65c4eccc7125ac06239e0
yamause <keitadep@gmail.com>
2020-05-20 08:12:55 +0900

    fixd bug

--- lambda/py/handler/handler_janken.py
+++ lambda/py/handler/handler_janken.py
@@ -24,19 +24,31 @@ class JankenIntentHandler(AbstractRequestHandler):
     def handle(self, handler_input):
                 
         class janken():
-            def main(cls,my_cmd,e,nemy_cmd):
+            @classmethod
+            def main(cls,my_cmd,enemy_cmd):
                 logger.debug("自分の手　" + str(my_cmd))
         
                 logger.debug("相手の手　" + str(enemy_cmd))
                 #ジャンケン処理
-                if enecotd == e   enemy_cmd == 1 and my_cmd == 2 or 
-                     s  plinnereturn(winner)
-               else :戦う
-                   winny_勝負,
-                        "デュエル",
-                class Dmaバトル)
-            def __in]inner,myfish_status,enemy_status):
-                    self.my_d = myfish_status["defense"]
+                if enemy_cmd == my_cmd:
+                    winner = "drow"
+                    return(winner)
+        
+                elif (
+                    enemy_cmd == 0 and my_cmd == 1 or 
+                    enemy_cmd == 1 and my_cmd == 2 or 
+                    enemy_cmd == 2 and my_cmd == 0
+                    ):
+                    winner = "enemy"
+                    return(winner)
+                else :
+                    winner = "my_fish"
+                    return(winner)
+                        
+        class DmageCalc():
+            def __init__(self,winner,myfish_status,enemy_status):
+                self.my_p = myfish_status["power"]
+                self.my_d = myfish_status["defense"]
                 self.my_l = myfish_status["life"]
 
                 self.en_p = enemy_status["power"]
@@ -81,7 +93,6 @@ class JankenIntentHandler(AbstractRequestHandler):
                 if my_status["life"] <= 0:
                     pers_attr["life"] = my_status["life"]
                     sess_attr["battle_cont"] = False
-                    sess_attr["JankenMode"] = False
                     speak_output = ("{}{}との戦いに敗北しました！").format(battleText,en_status["name"])
                     bools = True
 
@@ -89,13 +100,43 @@ class JankenIntentHandler(AbstractRequestHandler):
                     pers_attr["v_count"] += 1
                     pers_attr["life"] = my_status["life"]
                     sess_attr["battle_cont"] = False
-                    sess_attr["JankenMode"] = False
                     speak_output = ("{}{}との戦いに勝利しました！").format(battleText,en_status["name"])
                     bools = False
 
                 else :
                     pers_attr["life"] = my_status["life"]
-                    sess_attr["my_status"] = -
+                    sess_attr["my_status"] = my_status
+                    sess_attr["en_status"] = en_status
+                    sess_attr["battle_cont"] = True
+                    speak_output = ("{}次の戦闘に移ります").format(battleText)
+                    bools = False
+                    sess_attr["round"] += 1
+                    if sess_attr["round"] >= 3 :
+                        speak_output = ("{}決着がつかなかった。戦闘を終了します。").format(battleText)
+                return(speak_output,pers_attr,sess_attr,bools)
+#--------------------------------------------------------
+        class BattleInit:
+            @classmethod
+            def battleFunc(cls,slots_id,my_status,en_status):
+                # 相手と自分のコマンド入力
+                my_cmd = int(slots_id)
+                enemy_cmd = random.randint(0,2)
+                winner = janken.main(my_cmd,enemy_cmd)
+                # バトル計算
+                tmp = DmageCalc(winner,my_status,en_status)
+                my_status["life"] = tmp.my_l
+                en_status["life"] = tmp.en_l
+
+                return(
+                    result.result_method(
+                        my_status,
+                        en_status,
+                        pers_attr,
+                        sess_attr,
+                        tmp.myfishDamege,
+                        tmp.enemyDamege
+                    )
+                )
                 
         #-------------------------------------------------------
 
